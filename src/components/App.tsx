7e5bfcca9817ba1c8badbc0c1fcd930f1749eba4
Administrator <admin@example.com>
2021-01-04 17:32:50 +0900

    id, nameの管理方法修正

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import peer from '../skyway-config'
 import '../assets/App.css'
 import Main from './Main'
+import SignIn from './SignIn'
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <Main />
-    </div>
-  )
+  const [name, setName] = useState('')
+  const [localId, setLocalId] = useState('')
+
+  useEffect(() => {
+    if (peer) {
+      peer.once('open', (id) => setLocalId(id))
+    }
+  }, [localId])
+
+  if (name && peer && localId !== '') {
+    return <Main name={name} id={localId} />
+  }
+  return <SignIn setName={setName} />
 }
 
 export default App
